03e9cdc0eefcf651f4e4d5ea2a68e9cff931dc10
Norbert Ligas <norbert221198@gmail.com>
2021-06-04 23:25:41 +0200

    friday work

--- src/RLC.srcs/sim_1/new/RLC_coder_rtl_TB.sv
+++ src/RLC.srcs/sim_1/new/RLC_coder_rtl_TB.sv
@@ -18,31 +18,55 @@
 // Additional Comments:
 // 
 //////////////////////////////////////////////////////////////////////////////////
-
-
 module RLC_coder_rtl_TB;
-reg clock, reset, start;
-reg [31:0] data_in;
-reg [31:0] data_out;
 
-RLC_coder_rtl coder(clock, reset, start, data_in, data_out);
+reg clock, reset;
+reg in_valid = 1, out_ready = 1;
+wire in_ready, out_valid;
+integer test_terator = 0;
+reg [0:1][0:31] data_in_tmp = { 32'b1111111_000000000_11111_00000_1_0_1111, 
+                                32'b1010_1010_1010_1010_1010_1010_1010_1010 };
+reg [0:31] data_in = data_in_tmp[test_terator];
+wire [0:31] data_out;
+reg [0:1][0:31] data_out_correct = { 32'b10_101_01_111_10_011_01_011_11_00_10_010_000,
+                                     32'b10_101_01_111_10_011_01_011_11_00_10_010_000 };
+reg is_correct = 0;
 
-//Clock generator
+RLC_coder_rtl coder(clock, reset, in_valid, in_ready, out_valid, out_ready, data_in, data_out);
+
+
+// Clock generator
 initial
     clock <= 1'b1;
 always
     #5 clock <= ~clock;
  
- //Reset signal
+// Reset signal
 initial
 begin
     reset <= 1'b1;
     #10 reset <= 1'b0;
 end 
 
-initial
+// Input logic
+always_ff @ (posedge clock) 
 begin
-    start <= 1'b0;
-    #20 start <= 1'b1;
+    if (reset==1) 
+        in_valid<= 0;
+    else begin
+        in_valid<= 1;
+        if (in_valid==1 && in_ready==1)
+           data_in <= data_in_tmp[test_terator];
+   end
 end
+
+// Output logic
+always_ff @ (posedge clock) 
+begin
+    if (out_valid==1 && out_ready==1)
+        out_ready <= 0;
+    if(out_valid==1 && out_ready==0) 
+            $stop;
+end
+
 endmodule
--- src/RLC.srcs/sources_1/new/RLC_coder_rtl.sv
+++ src/RLC.srcs/sources_1/new/RLC_coder_rtl.sv
@@ -20,24 +20,133 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module RLC_coder_rtl(clock, reset, start, datain, dataout);
-    
+module RLC_coder_rtl(clock, reset, in_valid, in_ready, out_valid, out_ready, datain, dataout);
+
+parameter integer vector_size = 32;
+parameter integer letter_info_size = 2; // const parameter
+parameter integer letter_amount_size = 3; // const parameter (amount of bits + 2, because 0 and 1 are not possible)
 input clock;
 input reset;
-input start;
-input [31:0] datain;
-output [31:0] dataout;
+input in_valid;
+output reg in_ready;
+output reg out_valid;
+input out_ready;
+input [0:vector_size-1] datain;
+output reg [0:vector_size-1] dataout;
+
+// States
+reg compression_valid, compression_done;
+
+//Algorithm Variables
+reg [0:vector_size-1] a_datain; // input data
+reg [0:vector_size-1] a_dataout; // input data
 
 always @ (posedge clock)
 begin
     if(reset==1'b1)
     begin
-
+        in_ready <= 1;
+        out_valid <= 0;
+        dataout <= {vector_size{0}};
+        a_datain <= {vector_size{0}};
+        a_dataout <= {vector_size{0}};
+        compression_valid = 0;
     end
-    else
+    else if (in_ready==1 && in_valid==1)
     begin
-        
+        a_datain <= datain;
+        in_ready <= 0;
+        compression_valid <= 1;
+        compression_done <= 0;
+    end
+    else if(compression_done == 1)
+    begin
+        dataout <= a_dataout;
+        out_valid <= 1;
+    end
+    else if(out_valid == 1 && out_ready == 1)
+    begin
+        out_valid <= 0;
+        in_ready <= 1;
+        a_datain <= {vector_size{0}};
+        a_dataout <= {vector_size{0}};
     end
 end
+
+// compression state
+//reg [letter_info_size-1:0] letter_info;
+//reg [letter_amount_size-1:0] letter_amount;
+integer letter_amount = 0;
+integer letter = 0;
+reg over_size = 0;
+integer j = 1;
+integer i = 1;
+
+always_ff @ (posedge clock)
+begin
+    if(reset==1) 
+    begin
+        compression_done = 0;
+        letter_amount = 0;
+        letter = 0;
+    end
+    else if (compression_valid == 1) 
+    begin
+        letter = a_datain[0];
+        letter_amount = 1;
+               
+        for (i = 1, j = 1; i < vector_size; i++)
+        begin
+            if(letter == a_datain[i] && letter_amount < 9)
+                letter_amount++;
+            else
+            begin 
+                if (letter_amount == 1)
+                begin
+                    a_dataout[j +: letter_info_size] = (letter == 1'b1) ? 2'b11 : 2'b00;
+                    j = j + letter_info_size;
+                end
+                else
+                begin
+                    a_dataout[j +: letter_info_size] = (letter == 1'b1) ? 2'b10 : 2'b01; // 10 is 1, 01 is 0
+                    j = j + letter_info_size;
+                    a_dataout[j +: letter_amount_size] = letter_amount - 2; // TODO: hopes that it will cast automaticly
+                    j = j + letter_amount_size;
+                    letter_amount = 1;
+                end
+                letter = a_datain[i];
+            end
+            if (!(j < vector_size))
+            begin
+                over_size = 1;
+                break;
+            end
+        end
+        
+        if (!over_size)
+        begin
+            if (letter_amount == 1)
+            begin
+                a_dataout[j +: letter_info_size] = (letter == 1'b1) ? 2'b11 : 2'b00;
+                j = j + letter_info_size;
+            end
+            else
+            begin
+                a_dataout[j +: letter_info_size] = (letter == 1'b1) ? 2'b10 : 2'b01; // 10 is 1, 01 is 0
+                j = j + letter_info_size;
+                a_dataout[j +: letter_amount_size] = letter_amount - 2; // TODO: hopes that it will cast automaticly
+                j = j + letter_amount_size;
+                letter_amount = 1;
+            end
+            if (!(j < vector_size))
+                over_size = 1;
+        end
+        if (over_size)
+            a_dataout = a_datain;
+        // Flags
+        compression_valid = 0;
+        compression_done = 1;
+    end
+end 
 
 endmodule
