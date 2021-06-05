828e5f930bd974d5b870a6de76e43b7f3c131afc
Norbert Ligas <norbert221198@gmail.com>
2021-06-05 11:29:17 +0200

    vivado

--- src/RLC.srcs/sim_1/new/RLC_coder_rtl_TB.sv
+++ src/RLC.srcs/sim_1/new/RLC_coder_rtl_TB.sv
@@ -22,18 +22,19 @@ module RLC_coder_rtl_TB;
 
 reg clock, reset;
 reg in_valid = 1, out_ready = 1;
-wire in_ready, out_valid;
-integer test_terator = 0;
+wire in_ready, out_valid, oversize;
+integer test_iterator = 0;
 reg [0:1][0:31] data_in_tmp = { 32'b1111111_000000000_11111_00000_1_0_1111, 
                                 32'b1010_1010_1010_1010_1010_1010_1010_1010 };
-reg [0:31] data_in = data_in_tmp[test_terator];
+reg [0:31] data_in = data_in_tmp[test_iterator];
 wire [0:31] data_out;
 reg [0:1][0:31] data_out_correct = { 32'b10_101_01_111_10_011_01_011_11_00_10_010_000,
                                      32'b10_101_01_111_10_011_01_011_11_00_10_010_000 };
 reg is_correct = 0;
 
-RLC_coder_rtl coder(clock, reset, in_valid, in_ready, out_valid, out_ready, data_in, data_out);
+RLC_coder_rtl coder(clock, reset, in_valid, in_ready, out_valid, out_ready, oversize, data_in, data_out);
 
+reg set_reset = 0;
 
 // Clock generator
 initial
@@ -42,10 +43,16 @@ always
     #5 clock <= ~clock;
  
 // Reset signal
-initial
+always_ff @ (posedge clock)
 begin
-    reset <= 1'b1;
-    #10 reset <= 1'b0;
+    if(set_reset == 1'b1)
+    begin
+        reset <= 1'b1;
+        set_reset <= 1'b0;
+    end
+    else
+    if(reset == 1'b1 && set_reset == 1'b0)
+        reset <= 1'b0;
 end 
 
 // Input logic
@@ -56,7 +63,7 @@ begin
     else begin
         in_valid<= 1;
         if (in_valid==1 && in_ready==1)
-           data_in <= data_in_tmp[test_terator];
+           data_in <= data_in_tmp[test_iterator];
    end
 end
 
@@ -66,7 +73,12 @@ begin
     if (out_valid==1 && out_ready==1)
         out_ready <= 0;
     if(out_valid==1 && out_ready==0) 
-            $stop;
+    begin
+        $stop;
+        is_correct <= (data_out == data_out_correct[test_iterator]) ? 1'b1 : 1'b0;
+        ++test_iterator;
+        set_reset <= 1'b1;
+    end
 end
 
 endmodule
--- src/RLC.srcs/sources_1/new/RLC_coder_rtl.sv
+++ src/RLC.srcs/sources_1/new/RLC_coder_rtl.sv
@@ -20,7 +20,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module RLC_coder_rtl(clock, reset, in_valid, in_ready, out_valid, out_ready, datain, dataout);
+module RLC_coder_rtl(clock, reset, in_valid, in_ready, out_valid, out_ready, oversize, datain, dataout);
 
 parameter integer vector_size = 32;
 parameter integer letter_info_size = 2; // const parameter
@@ -30,6 +30,7 @@ input reset;
 input in_valid;
 output reg in_ready;
 output reg out_valid;
+output reg oversize;
 input out_ready;
 input [0:vector_size-1] datain;
 output reg [0:vector_size-1] dataout;
@@ -40,6 +41,7 @@ reg compression_valid, compression_done;
 //Algorithm Variables
 reg [0:vector_size-1] a_datain; // input data
 reg [0:vector_size-1] a_dataout; // input data
+reg a_oversize;
 
 always @ (posedge clock)
 begin
@@ -50,6 +52,7 @@ begin
         dataout <= {vector_size{0}};
         a_datain <= {vector_size{0}};
         a_dataout <= {vector_size{0}};
+        a_oversize <= 0;
         compression_valid = 0;
     end
     else if (in_ready==1 && in_valid==1)
@@ -62,12 +65,14 @@ begin
     else if(compression_done == 1)
     begin
         dataout <= a_dataout;
+        oversize <= a_oversize;
         out_valid <= 1;
     end
     else if(out_valid == 1 && out_ready == 1)
     begin
         out_valid <= 0;
         in_ready <= 1;
+        a_oversize <= 0;
         a_datain <= {vector_size{0}};
         a_dataout <= {vector_size{0}};
     end
@@ -95,7 +100,7 @@ begin
         letter = a_datain[0];
         letter_amount = 1;
                
-        for (i = 1, j = 1; i < vector_size; i++)
+        for (i = 1, j = 1; i < vector_size && a_oversize != 1; i++)
         begin
             if(letter == a_datain[i] && letter_amount < 9)
                 letter_amount++;
@@ -103,8 +108,56 @@ begin
             begin 
                 if (letter_amount == 1)
                 begin
+                    if (!(j < vector_size - letter_info_size))
+                    begin
+                        a_oversize = 1;
+                        break;
+                    end
+                    else
+                    begin
+                        a_dataout[j +: letter_info_size] = (letter == 1'b1) ? 2'b11 : 2'b00;
+                        j = j + letter_info_size;
+                    end
+                end
+                else
+                begin                        
+                    if (!(j < vector_size - letter_info_size - letter_amount_size))
+                    begin
+                        a_oversize = 1;
+                        break;
+                    end
+                    else
+                    begin
+                        a_dataout[j +: letter_info_size] = (letter == 1'b1) ? 2'b10 : 2'b01; // 10 is 1, 01 is 0
+                        j = j + letter_info_size;
+                        a_dataout[j +: letter_amount_size] = letter_amount - 2; // TODO: hopes that it will cast automaticly
+                        j = j + letter_amount_size;
+                        letter_amount = 1;
+                    end
+                end
+                letter = a_datain[i];
+            end
+        end
+        // Last letters addition
+        if (!a_oversize)
+        begin
+            if (letter_amount == 1)
+            begin
+                if (!(j < vector_size - letter_info_size))
+                begin
+                    a_oversize = 1;
+                end
+                else
+                begin
                     a_dataout[j +: letter_info_size] = (letter == 1'b1) ? 2'b11 : 2'b00;
                     j = j + letter_info_size;
+                end
+            end
+            else
+            begin                        
+                if (!(j < vector_size - letter_info_size - letter_amount_size))
+                begin
+                    a_oversize = 1;
                 end
                 else
                 begin
@@ -114,35 +167,8 @@ begin
                     j = j + letter_amount_size;
                     letter_amount = 1;
                 end
-                letter = a_datain[i];
-            end
-            if (!(j < vector_size))
-            begin
-                over_size = 1;
-                break;
             end
         end
-        
-        if (!over_size)
-        begin
-            if (letter_amount == 1)
-            begin
-                a_dataout[j +: letter_info_size] = (letter == 1'b1) ? 2'b11 : 2'b00;
-                j = j + letter_info_size;
-            end
-            else
-            begin
-                a_dataout[j +: letter_info_size] = (letter == 1'b1) ? 2'b10 : 2'b01; // 10 is 1, 01 is 0
-                j = j + letter_info_size;
-                a_dataout[j +: letter_amount_size] = letter_amount - 2; // TODO: hopes that it will cast automaticly
-                j = j + letter_amount_size;
-                letter_amount = 1;
-            end
-            if (!(j < vector_size))
-                over_size = 1;
-        end
-        if (over_size)
-            a_dataout = a_datain;
         // Flags
         compression_valid = 0;
         compression_done = 1;
